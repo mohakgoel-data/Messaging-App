768ebcf1e04c2ce16b9ff429a7eef7f4bacf59dd
mohakgoel-data <mohakgoel.data@gmail.com>
2026-02-10 21:01:41 +0530

    setup db layer-1

---
+++ app/models/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, String, Boolean, DateTime, func
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    phone_number = Column(String, unique=True, index=True, nullable=False)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=True)
+    about = Column(String, default="Hey there! I am a user.")
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
