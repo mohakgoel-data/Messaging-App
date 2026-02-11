fe693133bfef83d847a9bc50959c91405ffba144
Abhay Kumar Mishra <abhaykumarmishra0369@gmail.com>
2026-02-11 20:35:53 +0530

    add email field and update user activation logic

--- app/models/user.py
+++ app/models/user.py
@@ -7,9 +7,15 @@ class User(Base):
     __tablename__ = "users"
 
     user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+
+    email = Column(String, unique=True, index=True, nullable=False)
+
     phone_number = Column(String, unique=True, index=True, nullable=False)
+
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=True)
     about = Column(String, default="Hey there! I am a user.")
-    is_active = Column(Boolean, default=True)
+
+    is_active = Column(Boolean, default=False)
+
     created_at = Column(DateTime(timezone=True), server_default=func.now())
